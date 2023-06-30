154e8014bb3de8576cf6d0b21a504374964ac9d7
max-lec <maxime.leclerc.insa@gmail.com>
2023-06-30 17:39:19 +0200

    Fix : augment scrapper had problems with tier 1 & tier 2 + stat parsing

--- scrapper/scrap-augments.sh
+++ scrapper/scrap-augments.sh
@@ -24,7 +24,7 @@ AUGMENT_3_LIST=$(grep "guide-augments__title" "${DATA_PATH}/augments-3.html" | c
 readarray -t AUGMENT_3_ARRAY <<< $AUGMENT_3_LIST
 
 AUGMENT_ID=0
-for augment in "${AUGMENT_1_ARRAY[@]}" "${AUGMENT_2_ARRAY[@]}" "${AUGMENT_3_ARRAY[@]}"; do
+for augment in "${AUGMENT_1_ARRAY[@]}"; do
     echo "$augment"
     AUGMENT_NAME="$augment"
 
@@ -37,38 +37,38 @@ for augment in "${AUGMENT_1_ARRAY[@]}" "${AUGMENT_2_ARRAY[@]}" "${AUGMENT_3_ARRA
     curl -s "https:$AUGMENT_IMAGE_SRC" --output "./scrapper/../$AUGMENT_IMAGE_PATH"
 
     AUGMENT_STATS="{" 
-    if grep -e "gain[s]* [0-9]* [Hh]ealth" <(echo "$AUGMENT_DESC"); then
-        AUGMENT_STATS+='"health": '"$(echo "$AUGMENT_DESC" | sed -n 's|.*gain[s]* \([0-9]*\) [Hh]ealth.*|\1|p')"","
+    if grep -e "gain[s]* [0-9][0-9%.-]* [Hh]ealth" <(echo "$AUGMENT_DESC"); then
+        AUGMENT_STATS+='"health": '"$(echo "$AUGMENT_DESC" | sed 's|.*gain[s]* \([0-9%][0-9%.-]*\) [Hh]ealth.*|\1 |' | sed 's|.*-\([0-9%.-]*\).*|\1 |')"","
     else 
         AUGMENT_STATS+='"health": '"0,"
     fi
     if grep -e "[Aa]rmor[., ]" <(echo "$AUGMENT_DESC"); then
-        AUGMENT_STATS+='"armor": '"$(echo "$AUGMENT_DESC" | sed 's|.* \([0-9][0-9%]*\) .*[Aa]rmor.*|\1 |')"","
+        AUGMENT_STATS+='"armor": '"$(echo "$AUGMENT_DESC" | sed 's|.* \([0-9%][0-9%.-]*\) .*[Aa]rmor.*|\1 |')"","
     else 
         AUGMENT_STATS+='"armor": '"0,"
     fi
     if grep -e "[Mm]agic [Rr]esist[., ]" <(echo "$AUGMENT_DESC"); then
-        AUGMENT_STATS+='"resistance": '"$(echo "$AUGMENT_DESC" | sed 's|.* \([0-9][0-9%]*\) .*[Mm]agic [Rr]esist.*|\1 |')"","
+        AUGMENT_STATS+='"resistance": '"$(echo "$AUGMENT_DESC" | sed 's|.* \([0-9%][0-9%.-]*\) .*[Mm]agic [Rr]esist.*|\1 |')"","
     else 
         AUGMENT_STATS+='"resistance": '"0,"
     fi
     if grep -e "[0-9][0-9]* [Mm]ana" <(echo "$AUGMENT_DESC"); then
-        AUGMENT_STATS+='"mana": '"$(echo "$AUGMENT_DESC" | sed 's|.* \([0-9][0-9%]*\) .*[Mm]ana.*|\1 |')"","
+        AUGMENT_STATS+='"mana": '"$(echo "$AUGMENT_DESC" | sed 's|.* \([0-9%][0-9%.-]*\) .*[Mm]ana.*|\1 |')"","
     else 
         AUGMENT_STATS+='"mana": '"0,"
     fi
     if grep -e "Ability Power" <(echo "$AUGMENT_DESC"); then
-        AUGMENT_STATS+='"ap": '"$(echo "$AUGMENT_DESC" | sed 's|.* \([0-9][0-9%.]*\) .*Ability Power.*|\1 |')"","
+        AUGMENT_STATS+='"ap": '"$(echo "$AUGMENT_DESC" | sed 's|.* \([0-9%][0-9%.-]*\) .*Ability Power.*|\1 |')"","
     else 
         AUGMENT_STATS+='"ap": '"0,"
     fi
     if grep -e "Attack Damage" <(echo "$AUGMENT_DESC"); then
-        AUGMENT_STATS+='"attack": '"$(echo "$AUGMENT_DESC" | sed 's|.* \([0-9%][0-9%.]*\) .*Attack Damage.*|\1 |')"","
+        AUGMENT_STATS+='"attack": '"$(echo "$AUGMENT_DESC" | sed 's|.* \([0-9%][0-9%.-]*\) .*Attack Damage.*|\1 |')"","
     else 
         AUGMENT_STATS+='"attack": '"0,"
     fi
     if grep -e "Attack Speed" <(echo "$AUGMENT_DESC"); then
-        AUGMENT_STATS+='"speed": '"$(echo "$AUGMENT_DESC" | sed 's|.* \([0-9%][0-9%.-]*\) .*Attack Speed.*|\1 |')"","
+        AUGMENT_STATS+='"speed": '"$(echo "$AUGMENT_DESC" | sed 's|.* \([0-9%][0-9%.-]*\) .*Attack Speed.*|\1 |' | sed 's|.*-\([0-9%.-]*\).*|\1 |')"","
     else 
         AUGMENT_STATS+='"speed": '"0,"
     fi
@@ -91,3 +91,144 @@ for augment in "${AUGMENT_1_ARRAY[@]}" "${AUGMENT_2_ARRAY[@]}" "${AUGMENT_3_ARRA
                     }' > "${DATA_PATH}/${augment}.json"
         
 done
+
+
+
+for augment in "${AUGMENT_2_ARRAY[@]}"; do
+    echo "$augment"
+    AUGMENT_NAME="$augment"
+
+    AUGMENT_ID=$(( AUGMENT_ID + 1 ))
+
+    AUGMENT_DESC=$(grep -A 2 "$augment" "${DATA_PATH}/augments-2.html" | grep "guide-augments__desc" | sed -n 's|<p.*>\(.*\)</p>|\1|p' | xargs)
+    
+    AUGMENT_IMAGE_SRC=$(grep -B 2 "$augment" "${DATA_PATH}/augments-2.html" | grep "img src" | cut -d '"' -f 2)
+    AUGMENT_IMAGE_PATH="$IMAGE_PATH/${augment}.png"
+    curl -s "https:$AUGMENT_IMAGE_SRC" --output "./scrapper/../$AUGMENT_IMAGE_PATH"
+
+    AUGMENT_STATS="{" 
+    if grep -e "gain[s]* [0-9][0-9%.-]* [Hh]ealth" <(echo "$AUGMENT_DESC"); then
+        AUGMENT_STATS+='"health": '"$(echo "$AUGMENT_DESC" | sed 's|.*gain[s]* \([0-9%][0-9%.-]*\) [Hh]ealth.*|\1|' | sed 's|.*-\([0-9%.-]*\).*|\1 |' )"","
+    else 
+        AUGMENT_STATS+='"health": '"0,"
+    fi
+    if grep -e "[Aa]rmor[., ]" <(echo "$AUGMENT_DESC"); then
+        AUGMENT_STATS+='"armor": '"$(echo "$AUGMENT_DESC" | sed 's|.* \([0-9%][0-9%.-]*\) .*[Aa]rmor.*|\1 |')"","
+    else 
+        AUGMENT_STATS+='"armor": '"0,"
+    fi
+    if grep -e "[Mm]agic [Rr]esist[., ]" <(echo "$AUGMENT_DESC"); then
+        AUGMENT_STATS+='"resistance": '"$(echo "$AUGMENT_DESC" | sed 's|.* \([0-9%][0-9%.-]*\) .*[Mm]agic [Rr]esist.*|\1 |')"","
+    else 
+        AUGMENT_STATS+='"resistance": '"0,"
+    fi
+    if grep -e "[0-9][0-9]* [Mm]ana" <(echo "$AUGMENT_DESC"); then
+        AUGMENT_STATS+='"mana": '"$(echo "$AUGMENT_DESC" | sed 's|.* \([0-9%][0-9%.-]*\) .*[Mm]ana.*|\1 |')"","
+    else 
+        AUGMENT_STATS+='"mana": '"0,"
+    fi
+    if grep -e "Ability Power" <(echo "$AUGMENT_DESC"); then
+        AUGMENT_STATS+='"ap": '"$(echo "$AUGMENT_DESC" | sed 's|.* \([0-9%][0-9%.-]*\) .*Ability Power.*|\1 |')"","
+    else 
+        AUGMENT_STATS+='"ap": '"0,"
+    fi
+    if grep -e "Attack Damage" <(echo "$AUGMENT_DESC"); then
+        AUGMENT_STATS+='"attack": '"$(echo "$AUGMENT_DESC" | sed 's|.* \([0-9%][0-9%.-]*\) .*Attack Damage.*|\1 |')"","
+    else 
+        AUGMENT_STATS+='"attack": '"0,"
+    fi
+    if grep -e "Attack Speed" <(echo "$AUGMENT_DESC"); then
+        AUGMENT_STATS+='"speed": '"$(echo "$AUGMENT_DESC" | sed 's|.* \([0-9%][0-9%.-]*\) .*Attack Speed.*|\1 |' | sed 's|.*-\([0-9%.-]*\).*|\1 |')"","
+    else 
+        AUGMENT_STATS+='"speed": '"0,"
+    fi
+
+    AUGMENT_STATS=$(echo $AUGMENT_STATS | sed 's|%||g' | sed 's|.$||') # remove last comma and %
+    AUGMENT_STATS+="}"
+
+    jq -n \
+                  --arg name "$AUGMENT_NAME" \
+                  --arg id "$AUGMENT_ID" \
+                  --arg desc "$AUGMENT_DESC" \
+                  --arg srcPath "$AUGMENT_IMAGE_PATH" \
+                  --argjson stats "$AUGMENT_STATS" \
+                  '{ 
+                    name: $name, 
+                    id: $id, 
+                    desc: $desc,
+                    srcPath: $srcPath,
+                    stats: $stats
+                    }' > "${DATA_PATH}/${augment}.json"
+        
+done
+
+for augment in "${AUGMENT_3_ARRAY[@]}"; do
+    echo "$augment"
+    AUGMENT_NAME="$augment"
+
+    AUGMENT_ID=$(( AUGMENT_ID + 1 ))
+
+    AUGMENT_DESC=$(grep -A 2 "$augment" "${DATA_PATH}/augments-3.html" | grep "guide-augments__desc" | sed -n 's|<p.*>\(.*\)</p>|\1|p' | xargs)
+    
+    AUGMENT_IMAGE_SRC=$(grep -B 2 "$augment" "${DATA_PATH}/augments-3.html" | grep "img src" | cut -d '"' -f 2)
+    AUGMENT_IMAGE_PATH="$IMAGE_PATH/${augment}.png"
+    curl -s "https:$AUGMENT_IMAGE_SRC" --output "./scrapper/../$AUGMENT_IMAGE_PATH"
+
+    AUGMENT_STATS="{" 
+    if grep -e "gain[s]* [0-9][0-9%.-]* [Hh]ealth" <(echo "$AUGMENT_DESC"); then
+        AUGMENT_STATS+='"health": '"$(echo "$AUGMENT_DESC" | sed 's|.*gain[s]* \([0-9%][0-9%.-]*\) [Hh]ealth.*|\1|' | sed 's|.*-\([0-9%.-]*\).*|\1 |')"","
+    else 
+        AUGMENT_STATS+='"health": '"0,"
+    fi
+    if grep -e "[Aa]rmor[., ]" <(echo "$AUGMENT_DESC"); then
+        AUGMENT_STATS+='"armor": '"$(echo "$AUGMENT_DESC" | sed 's|.* \([0-9%][0-9%.-]*\) .*[Aa]rmor.*|\1 |')"","
+    else 
+        AUGMENT_STATS+='"armor": '"0,"
+    fi
+    if grep -e "[Mm]agic [Rr]esist[., ]" <(echo "$AUGMENT_DESC"); then
+        AUGMENT_STATS+='"resistance": '"$(echo "$AUGMENT_DESC" | sed 's|.* \([0-9%][0-9%.-]*\) .*[Mm]agic [Rr]esist.*|\1 |')"","
+    else 
+        AUGMENT_STATS+='"resistance": '"0,"
+    fi
+    if grep -e "[0-9][0-9]* [Mm]ana" <(echo "$AUGMENT_DESC"); then
+        AUGMENT_STATS+='"mana": '"$(echo "$AUGMENT_DESC" | sed 's|.* \([0-9%][0-9%.-]*\) .*[Mm]ana.*|\1 |')"","
+    else 
+        AUGMENT_STATS+='"mana": '"0,"
+    fi
+    if grep -e "Ability Power" <(echo "$AUGMENT_DESC"); then
+        AUGMENT_STATS+='"ap": '"$(echo "$AUGMENT_DESC" | sed 's|.* \([0-9%][0-9%.-]*\) .*Ability Power.*|\1 |')"","
+    else 
+        AUGMENT_STATS+='"ap": '"0,"
+    fi
+    if grep -e "Attack Damage" <(echo "$AUGMENT_DESC"); then
+        AUGMENT_STATS+='"attack": '"$(echo "$AUGMENT_DESC" | sed 's|.* \([0-9%][0-9%.-]*\) .*Attack Damage.*|\1 |')"","
+    else 
+        AUGMENT_STATS+='"attack": '"0,"
+    fi
+    if grep -e "Attack Speed" <(echo "$AUGMENT_DESC"); then
+        AUGMENT_STATS+='"speed": '"$(echo "$AUGMENT_DESC" | sed 's|.* \([0-9%][0-9%.-]*\) .*Attack Speed.*|\1 |' | sed 's|.*-\([0-9%.-]*\).*|\1 |' )"","
+    else 
+        AUGMENT_STATS+='"speed": '"0,"
+    fi
+
+    AUGMENT_STATS=$(echo $AUGMENT_STATS | sed 's|%||g' | sed 's|.$||') # remove last comma and %
+    AUGMENT_STATS+="}"
+
+    jq -n \
+                  --arg name "$AUGMENT_NAME" \
+                  --arg id "$AUGMENT_ID" \
+                  --arg desc "$AUGMENT_DESC" \
+                  --arg srcPath "$AUGMENT_IMAGE_PATH" \
+                  --argjson stats "$AUGMENT_STATS" \
+                  '{ 
+                    name: $name, 
+                    id: $id, 
+                    desc: $desc,
+                    srcPath: $srcPath,
+                    stats: $stats
+                    }' > "${DATA_PATH}/${augment}.json"
+        
+done
+### Concat final stats file 
+
+jq -s . $DATA_PATH/*.json > $DATA_PATH/final/stats.json
