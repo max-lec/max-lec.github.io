3c76b067149832a0a0bdcfeaef564fed27e8aac7
max-lec <maxime.leclerc.insa@gmail.com>
2023-07-04 23:36:51 +0200

    Feat : added simple time to cast /15s /30s (no mana lock yet)

--- scripts/cast_stats.js
+++ scripts/cast_stats.js
@@ -1,9 +1,4 @@
-function computeFirstCast(){
-    let manaToCast = Alpine.store('stats').manaCast - Alpine.store('stats').manaStart
-    let manaPerAuto = [Alpine.store('stats').manaGen[0], Alpine.store('stats').manaGen[1]*100]; // [10.5, 0.7]
-    let augmentBonus = [Alpine.store('currentAugment').getTotalManaBonus()[0], Alpine.store('currentAugment').getTotalManaBonus()[1]*100]; // [20, 5]
-    let traitBonus = [Alpine.store('currentTrait').getTotalManaBonus()[0], Alpine.store('currentTrait').getTotalManaBonus()[1]*100]; // [15, 3]
-
+function computeCast(manaToCast, manaPerAuto, augmentBonus, traitBonus){
     tick=1
     while(manaToCast > 0 && tick < 3000) {
 
@@ -22,4 +17,32 @@ function computeFirstCast(){
         tick += 1;
     }
     return Math.round(tick/100 *10)/10;
+}
+
+function computeFirstCast(){
+    let manaToCast = Alpine.store('stats').manaCast - Alpine.store('stats').manaStart
+    let manaPerAuto = [Alpine.store('stats').manaGen[0], Alpine.store('stats').manaGen[1]*100]; // [10.5, 0.7]
+    let augmentBonus = [Alpine.store('currentAugment').getTotalManaBonus()[0], Alpine.store('currentAugment').getTotalManaBonus()[1]*100]; // [20, 5]
+    let traitBonus = [Alpine.store('currentTrait').getTotalManaBonus()[0], Alpine.store('currentTrait').getTotalManaBonus()[1]*100]; // [15, 3]
+    return computeCast(manaToCast, manaPerAuto, augmentBonus, traitBonus);
+}
+
+function computeNextCast(){
+    let manaToCast = Alpine.store('stats').manaCast
+    let manaPerAuto = [Alpine.store('stats').manaGen[0], Alpine.store('stats').manaGen[1]*100]; // [10.5, 0.7]
+    let augmentBonus = [Alpine.store('currentAugment').getTotalManaBonus()[0], Alpine.store('currentAugment').getTotalManaBonus()[1]*100]; // [20, 5]
+    let traitBonus = [Alpine.store('currentTrait').getTotalManaBonus()[0], Alpine.store('currentTrait').getTotalManaBonus()[1]*100]; // [15, 3]
+    return computeCast(manaToCast, manaPerAuto, augmentBonus, traitBonus);
+}
+
+function computeCastsPer15(){
+    let timeAfterFirstCast = 15 - computeFirstCast();
+    let timeToCast = computeNextCast();
+    return Math.floor(timeAfterFirstCast / timeToCast) + 1;
+}
+
+function computeCastsPer30(){
+    let timeAfterFirstCast = 30 - computeFirstCast();
+    let timeToCast = computeNextCast();
+    return Math.floor(timeAfterFirstCast / timeToCast) + 1;
 }
