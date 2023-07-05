f84b0f7223326de0f6d469a84f8914cde54c9726
max-lec <maxime.leclerc.insa@gmail.com>
2023-07-05 13:05:15 +0200

    Feat : added simple damage calc with ad/ap ratio + multihit

--- scripts/damage_stats.js
+++ scripts/damage_stats.js
@@ -1,7 +1,49 @@
+
+
 function computeAttackDPS() {
     return Alpine.store('stats').attack * Alpine.store('stats').speed/100
 }
 
+
 function computeAbilityDamage() {
-    
+    let damageSpells = []
+    Alpine.store('currentChampion').spells.forEach(spell => {
+        if(spell.type == "damage") {
+            damageSpells.push(spell);
+        }
+    });
+
+    let abilityDamages = []
+    damageSpells.forEach(spell => {
+        let rank = Alpine.store('currentChampion').level -1;
+        let apDamage = spell.apRatio[rank] + (spell.apRatio[rank] * Alpine.store('stats').ap/100);
+        let attackDamage = Alpine.store('stats').attack * spell.attackRatio[rank]/100;
+
+        let res = (apDamage + attackDamage)
+        abilityDamages.push(res);
+    });
+
+    return abilityDamages;
+}
+
+
+function computeAbilityDamageMultihit() {
+    let damageSpells = []
+    Alpine.store('currentChampion').spells.forEach(spell => {
+        if(spell.type == "damage") {
+            damageSpells.push(spell);
+        }
+    });
+
+    let abilityDamages = []
+    damageSpells.forEach(spell => {
+        let rank = Alpine.store('currentChampion').level -1;
+        let apDamage = spell.apRatio[rank] + (spell.apRatio[rank] * Alpine.store('stats').ap/100);
+        let attackDamage = Alpine.store('stats').attack * spell.attackRatio[rank]/100;
+        let multihit = Alpine.store('options').multihit <= spell.maxMultihit ? Alpine.store('options').multihit : spell.maxMultihit ;
+        let res = (apDamage + attackDamage) * multihit;
+        abilityDamages.push(res);
+    });
+
+    return abilityDamages;
 }
